c142aa96b8afdc0cce78953461ec75bea5830b3a
TomasCroci <83694309+TomasCroci@users.noreply.github.com>
2021-12-10 19:43:12 -0300

    Delete TP2_Servlet directory

--- TP2_Servlet/src/main/java/ar/org/centro8/curso/java/web/enums/EspecieRecomendada.java
+++
@@ -1,3 +0,0 @@
-package ar.org.centro8.curso.java.web.enums;
-
-public enum EspecieRecomendada {CANINO,FELINO,AVE,PEZ,ROEDOR}
--- TP2_Servlet/src/main/java/ar/org/centro8/curso/java/web/enums/TipoDocumento.java
+++
@@ -1,9 +0,0 @@
-package ar.org.centro8.curso.java.web.enums;
-
-public enum TipoDocumento {
-    DNI,
-    LC,
-    LE,
-    CI,
-    PASS
-}
